9a5b21577350e467bdbd1a4aad7ae37b8815d94f
gautam <support@rohininfotech.com>
2022-11-30 13:06:33 +0530

    gautam costing

--- routes/sales.js
+++ routes/sales.js
@@ -1,4 +1,17 @@
 const salesRoute=require("express").Router();
 
+const { browseCosting, currentFy, previewCostingTabs, generateCostNo, pickcCosting, getCosting } = require("../controllers/sales/costing");
+
+
+salesRoute.post('/browse_cos',browseCosting)
+
+salesRoute.post("/current_fy",currentFy)
+salesRoute.post("/preview_costing",previewCostingTabs)
+salesRoute.post("/generate_cost",generateCostNo)
+salesRoute.post("/pick_cost",pickcCosting)
+salesRoute.post("/get_cost",getCosting)
+
+
+
 
 module.exports={salesRoute};
